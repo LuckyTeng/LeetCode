9c537128fbd3ede479105e08259a4eca9e93db25
teng <holy_necklace@yahoo.com.tw>
2022-01-02 15:08:55 +0800

    Add two sum test.

--- Java/SolutionTest.java
+++ Java/SolutionTest.java
@@ -1,8 +1,17 @@
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Test;
 
 public class SolutionTest {
     @Test
     public void testTwoSum() {
+        int[] nums = new int[] { 2, 7, 11, 5};
+        int target = 9;
+
+        Solution s = new Solution();
         
+        var actuals = s.twoSum(nums, target);
+        int[] expecteds = new int[] { 0, 1 };
+        assertArrayEquals(expecteds, actuals);
     }
 }
