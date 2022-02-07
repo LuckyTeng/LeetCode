34a0c10a41356be76517e82b89f568e9352e7afc
teng <holy_necklace@yahoo.com.tw>
2022-02-08 00:30:38 +0800

    Add test for encode decode.

---
+++ Java/test/SolutionEncodeandDecodeStringsTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import src.SolutionEncodeandDecodeStrings;
+
+public class SolutionEncodeandDecodeStringsTest {
+    @Test
+    public void test() {
+        var sol = new SolutionEncodeandDecodeStrings();
+
+        List<String> strs = new ArrayList<>();
+        strs.add("abc");
+        strs.add("mpg");
+        
+        var en = sol.encode(strs);
+        var actual = sol.decode(en);
+
+        assertEquals("abc", actual.get(0));
+        assertEquals("mpg", actual.get(1));
+    }
+}
