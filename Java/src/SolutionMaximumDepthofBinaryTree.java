97d118f808a06e2fcd622e0bd9c10a619e2e0b48
teng <holy_necklace@yahoo.com.tw>
2022-04-21 06:20:50 +0800

    Add maxDepthBFS.

--- Java/src/SolutionMaximumDepthofBinaryTree.java
+++ Java/src/SolutionMaximumDepthofBinaryTree.java
@@ -1,5 +1,9 @@
 package src;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+@LeetCodeInformation(Number = 104, Url = "https://leetcode.com/problems/maximum-depth-of-binary-tree/")
 public class SolutionMaximumDepthofBinaryTree {
     public class TreeNode {
         int val;
@@ -14,21 +18,53 @@ public class SolutionMaximumDepthofBinaryTree {
         }
     }
 
+    int method = 0;
     public int maxDepth(TreeNode root) {
         if ( root == null )
             return 0;
         
-        return maxDepth(root, 1);
+        if ( method == 0)
+            return maxDepthBFS(root);
+        else
+            return maxDepthRecur(root, 1);
     }
 
-    private int maxDepth(TreeNode root, int deep) {
+    private int maxDepthBFS(TreeNode root) {
+        int ans = 1;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (q.size() > 0) {
+            Queue<TreeNode> temp = new LinkedList<>();
+            boolean hasChild = false;
+            while (q.size() > 0) {
+                TreeNode node = q.poll();
+
+                if (node.left != null) {
+                    temp.offer(node.left);
+                    hasChild = true;
+                }
+                if (node.right != null) {
+                    temp.offer(node.right);
+                    hasChild = true;
+                }
+            }
+            if (hasChild)
+                ans++;
+            q = temp;
+        }
+
+        return ans;
+    }
+
+    private int maxDepthRecur(TreeNode root, int deep) {
         if ( root.left == null && root.right == null ) return deep;
 
         int nextLevel = deep + 1;
         if ( root.left != null )
-            deep = maxDepth(root.left, nextLevel);
+            deep = maxDepthRecur(root.left, nextLevel);
         if ( root.right != null )
-            deep = Math.max(maxDepth(root.right, nextLevel), deep); // deep is max from left, so select max
+            deep = Math.max(maxDepthRecur(root.right, nextLevel), deep); // deep is max from left, so select max
 
         return deep;
     }
